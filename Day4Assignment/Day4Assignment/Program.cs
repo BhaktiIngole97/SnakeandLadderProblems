131d999c4ac97e575048c90e61decc218466c75b
Your BhaktiIngole97 <ingolebhakti97@gmail.com>
2022-07-18 19:36:57 +0530

    UC-7-PlayerGame

--- Day4Assignment/Day4Assignment/Program.cs
+++ Day4Assignment/Day4Assignment/Program.cs
@@ -1,4 +1,19 @@
-﻿using Day4Assignment;
-Addplayer play = new Addplayer();
+﻿
+
+using Day4Assignment;
+Player play = new Player();
 Console.WriteLine("Welcome to snake and ladder problem.");
-play.gamePlay();
+int player1 = play.gamePlay(1);
+int player2 = play.gamePlay(2);
+if (player1 < player2)
+{
+    Console.WriteLine("Player 1 is a Winner.");
+}
+else if (player1 > player2)
+{
+    Console.WriteLine("Player 2 is a Winner.");
+}
+else
+{
+    Console.WriteLine("Its a tie between Player 1 and 2.");
+}
