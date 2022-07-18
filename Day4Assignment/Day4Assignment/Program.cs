a4031f83a95aad4f1ca5e2b1a1397a4b0ff1c1c9
Your BhaktiIngole97 <ingolebhakti97@gmail.com>
2022-07-18 19:03:28 +0530

    UC-6-DieRole

--- Day4Assignment/Day4Assignment/Program.cs
+++ Day4Assignment/Day4Assignment/Program.cs
@@ -1,40 +1,4 @@
-﻿const int LADDER = 1;
-const int SNAKE = 2;
-const int WINNING_POINT = 100;
-Console.WriteLine("Welcome to snake and ladder problem.");
-int tempPosition;
-for (int position = 0; position <= WINNING_POINT; position++)
-{
-    Random random = new Random();
-    int option = random.Next(0, 3);
-    int rolldie = random.Next(1, 7);
-    switch (option)
-    {
-        case LADDER:
-            tempPosition = position + rolldie;
-            if (tempPosition > 100)
-            {
-                position += 0;
-            }
-            else
-            {
-                position = tempPosition;
-            }
-            break;
-        case SNAKE:
-            tempPosition = position - rolldie;
-            if (tempPosition <= 0)
-            {
-                position = 0;
-            }
-            else
-            {
-                position = tempPosition;
-            }
-            break;
-        default:
-            position += 0;
-            break;
-    }
-    Console.WriteLine("Player 1 position is " + position);
-}
+﻿using Day4Assignment;
+Addplayer play = new Addplayer();
+Console.WriteLine("Welcome .");
+play.gamePlay();
