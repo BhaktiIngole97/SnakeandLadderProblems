98591229fa5153c11f119fe0e21d5731e0c85b33
Your BhaktiIngole97 <ingolebhakti97@gmail.com>
2022-07-18 17:04:21 +0530

    UC-2-Rolls the die

--- Day4Assignment/Day4Assignment/Program.cs
+++ Day4Assignment/Day4Assignment/Program.cs
@@ -1,3 +1,6 @@
-﻿Console.WriteLine("Welcome to snake and ladder problem");
+﻿Console.WriteLine("Welcome to Snake & Ladder problem.");
 int position = 0;
+Random random = new Random();
+int rolldie = random.Next(1, 7);
 Console.WriteLine("Player 1 position is " + position);
+Console.WriteLine("Roll Die value: " + rolldie);
